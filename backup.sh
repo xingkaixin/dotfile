94b24907c0e220b654904ffabcc3bafff840a6a4
XingKaiXin <xingkaixin@gmail.com>
2020-08-11 15:38:32 +0800

    init

---
+++ backup.sh
@@ -0,0 +1,4 @@
+
+cp ~/Library/Application\ Support/Code/User/settings.json  ~/Dropbox/01_个人资料/00_dotfiles/vscode/
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings  ~/Dropbox/01_个人资料/00_dotfiles/sublime/
+cp ~/.zshrc ~/Dropbox/01_个人资料/00_dotfiles/_zshrc/macOS.zshrc
