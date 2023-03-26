989f88bcab224b2e9fcff4fad3150e13f47fa0ad
XingKaiXin <xingkaixin@gmail.com>
2023-03-26 16:16:02 +0800

    add powershell

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,32 @@
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+# Prompt
+Import-Module posh-git
+$omp_config = Join-Path $PSScriptRoot ".\kevin.omp.json"
+oh-my-posh init pwsh --config $omp_config | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
